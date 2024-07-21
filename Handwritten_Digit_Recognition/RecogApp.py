d0eb37e1a9a5c238b571d03f8f1a148cb87ebef1
Aditya-pro2 <76590375+Aditya-pro2@users.noreply.github.com>
2024-07-21 10:34:52 +0530

    Update RecogApp.py

--- Handwritten_Digit_Recognition/RecogApp.py
+++ Handwritten_Digit_Recognition/RecogApp.py
@@ -8,7 +8,7 @@ st.title(":blue[HANDWRITTEN DIGIT RECOGNIZER]")
 
 @st.cache(allow_output_mutation = True)
 def load_model():
-    model=tf.keras.models.load_model('models/Digit Recognizer.keras')
+    model=tf.keras.models.load_model('models/Digit Recognizer.keras', compile = False)
     return model
 
 with st.spinner("Loading Model...."):
