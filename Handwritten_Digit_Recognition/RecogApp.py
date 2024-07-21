3cd543ff9c9c6d6128c7dc90894f2d164bf1ce88
Aditya-pro2 <76590375+Aditya-pro2@users.noreply.github.com>
2024-07-21 12:21:10 +0530

    Update RecogApp.py

--- Handwritten_Digit_Recognition/RecogApp.py
+++ Handwritten_Digit_Recognition/RecogApp.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 st.title(":blue[HANDWRITTEN DIGIT RECOGNIZER]")
 
-model = tf.keras.models.load_model("C:\Users\adity\DigitRecognizer.keras")
+model = tf.keras.models.load_model(r"C:\Users\adity\DigitRecognizer.keras")
 
 st.header("Write a digit below:")
 
